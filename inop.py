afed862d13679172d81bc611fa5cf793c3417e0f
Navarro <johnnyminemonic@gmail.com>
2020-11-02 14:25:58 -0300

    fluxo de variaveis Novas Telas Perguntas Senso

--- inop.py
+++ inop.py
@@ -10,10 +10,12 @@ resposta = ler.readlines()
 ler.close()
 
 class Tela1(Screen):
+
     prefeito = StringProperty()
     def imprimir(self):
         print("candidato:".join([self.prefeito]))
-        pass
+
+
 class Tela2(Screen):
     pass
 
@@ -22,12 +24,30 @@ class Tela3(Screen):
     pass
 
 
+class Tela4(Screen):
+    pass
+
+class Tela5(Screen):
+    pass
+
+
+class Tela6(Screen):
+    pass
+
+
+class Tela7(Screen):
+    pass
+
+
 # Create the screen manager
 sm = ScreenManager()
 sm.add_widget(Tela1(name='tela1'))
 sm.add_widget(Tela2(name='tela2'))
 sm.add_widget(Tela3(name='tela3'))
-
+sm.add_widget(Tela4(name='tela4'))
+sm.add_widget(Tela5(name='tela5'))
+sm.add_widget(Tela6(name='tela6'))
+sm.add_widget(Tela7(name='tela7'))
 
 class DemoApp(MDApp):
 
